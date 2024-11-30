d6e3cbcf48733dd29a43be4f23ca496276fdcbb6
Amy Gray <150024764+amy-amdigital@users.noreply.github.com>
2024-11-30 15:46:30 +0000

    Install playwright and setup config

---
+++ playwright.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, devices } from '@playwright/test';
+import { defineBddConfig } from 'playwright-bdd';
+
+const testDir = defineBddConfig({
+  features: 'features/*.feature',
+  steps: 'steps/*.steps.ts',
+});
+
+export default defineConfig({
+  testDir,
+  use: {
+    baseURL: 'https://demo.quartexcollections.com/',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+});
